47d12824a7320db6f12128a23e9f296f58345666
JijoNasrun <jijohakim@gmail.com>
2015-10-12 22:04:51 +0800

    assignment2

--- assignment2_1.c
+++ assignment2_1.c
@@ -24,12 +24,30 @@ int main()
 	{	perror("bind malfunction:"); exit(-1);	}
 	
 	//setting the socket's send buffer and receiving buffer//
+	
+	a= getsockopt(sock,SOL_SOCKET,SO_SNDBUF,0,0);
+	printf("SEND BUFFER BEFORE:%d\n",a);
+	
 	val=32768;
 	a = setsockopt( sock,SOL_SOCKET,SO_SNDBUF,(void *)& val,sizeof(val));
 	
+	printf("SEND BUFFER AFTER : %d\n", a);
+	
+	a = setsockopt(sock,SOL_SOCKET,SO_RCVBUF,0,0);
+	printf("RECEIVING BUFFER BEFORE: %d\n",a);
 	a = setsockopt( sock,SOL_SOCKET,SO_RCVBUF,(void *)& val,sizeof(val));
+	
+	printf("RECEIVING BUFFER AFTER: %d\n",a);
 	struct timeval time;
 	socklen_t len2; 
+	
 	a = getsockopt(sock ,SOL_SOCKET,SO_SNDTIMEO,(void *)& time,&len2);
 	printf("timeout %d seconds, %ld microseconds\n",time.tv_sec,time.tv_usec);
+	
+	int x=getsockopt(sock,SOL_SOCKET,SO_REUSEADDR,0,0);
+	
+	printf("REUSEADDR BEFORE :%d\n",x);
+	int on=1;
+	x=setsockopt(sock,SOL_SOCKET,SO_REUSEADDR,(void *)&on,sizeof(on));
+	printf("REUSEADDR AFTER:%d\n",x);
 }

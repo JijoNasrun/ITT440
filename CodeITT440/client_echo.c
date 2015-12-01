f068398538fcf0001fdb64e8b4ad10befca93b4d
JijoNasrun <jijohakim@gmail.com>
2015-12-01 15:02:23 +0800

    Project

--- CodeITT440/client_echo.c
+++ CodeITT440/client_echo.c
@@ -7,7 +7,9 @@
 #include<unistd.h>
 #include<arpa/inet.h>
 #include<string.h>
-
+#include<sys/wait.h>
+#include<ctype.h>
+#include<netdb.h>
 int main(int argc, char *argv[])
 {
 	int sock;
@@ -15,6 +17,7 @@ int main(int argc, char *argv[])
 	char input[1024];
 	char output[1024];
 	int len;
+	char revbuf[1024];
 	
 	sock=socket(AF_INET,SOCK_STREAM,0);
 	if(sock==-1)
@@ -28,15 +31,42 @@ int main(int argc, char *argv[])
 	if(connect(sock,(struct sockaddr *)&remote_server,sizeof(struct sockaddr_in))==-1)
 	{perror("connect:");exit(-1);}
 	
-	while(1)
+	//getting file from the server
+	printf("Receiving file from Server.....");
+	char *fr_name ="receivedClient.txt";
+	FILE *fr = fopen(fr_name, "abc");
+	if(fr == NULL)
 	{
-		fgets(input,1024,stdin);
-		send(sock,input,strlen(input),0);
-	
-		len=recv(sock,output,1024,0);
-		output[len]='\0';
-		printf("%s\n",output);
-
+		printf("File Cannot be Opened\n");
 	}
+	else
+	{
+		bzero(revbuf,1024);
+		int fr_block_sz =0;
+		
+		while((fr_block_sz = recv (sock, revbuf, 1024,0)) > 0)
+		{
+			int write_sz = fwrite(revbuf, sizeof(char),fr_block_sz, fr);
+			if (write_sz <fr_block_sz)
+			{
+				perror("File write failed.\n");
+			}
+			bzero(revbuf,1024);
+			if (fr_block_sz <0 )
+		{
+			if(errno==EAGAIN)
+			{
+				printf("recv() timed out\n");
+			}
+			else
+			{
+				fprintf(stderr, "recv() dailed due to errno = %d",errno);
+			}
+		}
+		printf("OK received from server\n");
+		fclose(fr);
+		
 	close(sock);
+}
+}
 }
